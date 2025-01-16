86491e5ba78a8329df6a349f0e883e1cd1318630
AmirrezaAhmadi <AmirrezaAhmadi.GH@gmail.com>
2025-01-16 22:51:08 +0330

    middlewares added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const userController = require("../controller/userController");
+const isAuthenticated = require("../middlewares/isAuth");
+
 const router = express.Router();
 
 router.post("/register", userController.register);
 
 router.get("/login", userController.login);
 
-router.get("/profile", userController.profile);
+router.get("/profile", isAuthenticated ,userController.profile);
 
 module.exports = router;
